837802e4744cbb24e9bbc22659b01c59027ac33d
Mykhailo Iskra <iskramichel@gmail.com>
2020-11-21 15:34:05 +0200

    Добавляет код Юрия по работе с локалстореджем

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import movieCards from './templates/movie-card.hbs';
 
 const apiService = new ApiService;
 const moviesList = document.querySelector('.home-list');
-/ Необходимо повесить дата атрибут data-action="add-to-watched" на одноименную кнопку фильма,
+
+// Необходимо повесить дата атрибут data-action="add-to-watched" на одноименную кнопку фильма,
 // data-action="add-to-queue" на одноименную кнопку фильма.
 // на эти же кнопки необходимо записывать в data-id - id открытого фильма
-const refs = {btnAddToWatched : document.querySelector('[data-action="add-to-watched"]'),
+
+const refs = {
+    btnAddToWatched: document.querySelector('[data-action="add-to-watched"]'),
 btnAddToQueue : document.querySelector('[data-action="add-to-queue"]')};
 let watchedArray = localStorage.getItem('WATCHED_KEY') ? JSON.parse(localStorage.getItem('WATCHED_KEY')) : [];
 let queueArray = localStorage.getItem('QUEUE_KEY') ? JSON.parse(localStorage.getItem('QUEUE_KEY')) : [];
@@ -16,7 +19,6 @@ let queueArray = localStorage.getItem('QUEUE_KEY') ? JSON.parse(localStorage.get
 
 window.addEventListener('load', onLoad());
 
-/
 // слушатели добавлять при открытии большой карточки фильма и снимать при закрытии
 // при нажимании на любую из этих кнопок она далжна становиться неактивной
 refs.btnAddToWatched.addEventListener('click', addToWatched);
@@ -45,27 +47,29 @@ function clearMarkup() {
 
 
 function renderGenres(genres, movies) {
-  return movies.map(({ genre_ids, ...otherProps }) => {
-    const genre_names = genre_ids.map((genreId) => {
-      const { name } = genres.find(({ id }) => id === genreId);
-      return name;
+    return movies.map(({ genre_ids, ...otherProps }) => {
+        const genre_names = genre_ids.map((genreId) => {
+            const { name } = genres.find(({ id }) => id === genreId);
+            return name;
+        });
+        return { ...otherProps, genre_names };
     });
-    return { ...otherProps, genre_names };
-  });
-  function addToWatched(e){
-    const filmName = e.currentTarget.dataset.id;
-
-     watchedArray = [watchedArray];
-    watchedArray.push(`${filmName}`);
-    
-    localStorage.setItem('WATCHED_KEY', JSON.stringify(`${watchedArray}`));
 }
-function addToQueue(e){
-    const filmName = e.currentTarget.dataset.id;
-    
-    queueArray = [queueArray];
-    queueArray.push(`${filmName}`);
-    
-    localStorage.setItem('QUEUE_KEY', JSON.stringify(`${queueArray}`));}
 
+function addToWatched(e) {
+        const filmName = e.currentTarget.dataset.id;
+
+        watchedArray = [watchedArray];
+        watchedArray.push(`${filmName}`);
+    
+        localStorage.setItem('WATCHED_KEY', JSON.stringify(`${watchedArray}`));
+    }
+function addToQueue(e) {
+        const filmName = e.currentTarget.dataset.id;
+    
+        queueArray = [queueArray];
+        queueArray.push(`${filmName}`);
+    
+        localStorage.setItem('QUEUE_KEY', JSON.stringify(`${queueArray}`))
+    }
 
