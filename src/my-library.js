7a26c3b50d6166c914852ba709c1792bedb9d18b
o-naichenko <o.naichenko@gmail.com>
2020-11-22 15:18:26 +0200

    додає шаблон для переглянутих фільмів

--- src/my-library.js
+++ src/my-library.js
@@ -6,37 +6,34 @@ const apiService = new ApiService();
 // const paginationContainer = document.getElementsByClassName(
 //   'pagination-buttons',
 // )[0];
-import movieCards from './templates/movie-card.hbs';
+import movieCards from './templates/movie-card-by-id.hbs';
 
 let watchedMovies = [];
 
 const refs = {
-    btnWatched : document.querySelector('[data-action="watched"]'),
-    btnQueue : document.querySelector('[data-action="queue"]'),
-    moviesList : document.querySelector('.home-list'),
-}
+  btnWatched: document.querySelector('[data-action="watched"]'),
+  btnQueue: document.querySelector('[data-action="queue"]'),
+  moviesList: document.querySelector('.home-list'),
+};
 
 refs.btnWatched.addEventListener('click', showWatched);
 // refs.btnQueue.addEventListener('click', showQueue);
 
 function makeMovieCardsMarkup(results) {
   const markup = movieCards(results);
-    refs.moviesList.innerHTML = markup;
-  }
+  refs.moviesList.innerHTML = markup;
+}
 
-async function showWatched(){
-    let watchedArray = localStorage.getItem('WATCHED_KEY')
-  ? JSON.parse(localStorage.getItem('WATCHED_KEY'))
-  : [];
+async function showWatched() {
+  let watchedArray = localStorage.getItem('WATCHED_KEY')
+    ? JSON.parse(localStorage.getItem('WATCHED_KEY'))
+    : [];
 
-  for (let i = 0; i < watchedArray.length; i += 1){
-    
-    await apiService.fetchMovieById(Number(watchedArray[i])).then(result => watchedMovies.push(result));
-    
-
+  for (let i = 0; i < watchedArray.length; i += 1) {
+    await apiService
+      .fetchMovieById(Number(watchedArray[i]))
+      .then(result => watchedMovies.push(result));
   }
   console.log(watchedMovies);
   makeMovieCardsMarkup(watchedMovies);
 }
-
-
